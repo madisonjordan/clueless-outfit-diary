0ea9f5903bac2473c4c87e04d91c8b1a9125f09c
cbril <cbril@csu.fullerton.edu>
2019-04-07 11:37:01 -0700

    Basic new outfit selections

--- android/app/src/main/java/com/clueless/MainApplication.java
+++ android/app/src/main/java/com/clueless/MainApplication.java
@@ -3,6 +3,7 @@ package com.clueless;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new AsyncStoragePackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
